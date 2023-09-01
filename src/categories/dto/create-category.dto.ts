eda730054e7d2daa708c939293dfbc416d3c9cbc
Pinita Promthep <sasuke_kub@hotmail.com>
2023-09-01 20:09:20 +0700

    update product entity

--- src/categories/dto/create-category.dto.ts
+++ src/categories/dto/create-category.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsInt, IsString, Length } from 'class-validator';
+import { IsString, Length } from 'class-validator';
 
 export class CreateCategoryDto {
   @ApiProperty()
